f48065d4234b1ef7fd0480ba61f50a8c4761e95d
Hannes <hannes@rang.de>
2019-08-14 23:18:36 +0200

    a lot of date extraction and conversion

--- src/daily_dish_extractor.rs
+++ src/daily_dish_extractor.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
 extern crate chrono;
-use chrono::NaiveDate;
+use chrono::prelude::*;
+use chrono::{Duration, NaiveDate};
 
 fn extract_weekly_dishes(preformatted_menu: Vec<&str>) -> Vec<&str> {
     let index_of_taeglich = preformatted_menu
@@ -40,16 +41,56 @@ fn extract_daily_dishes(preformatted_menu: Vec<&str>) -> HashMap<String, String>
     return dishes_by_day;
 }
 
-fn get_date(dayAndMonth: &str, year: u16) -> NaiveDate {
-    if dayAndMonth == "5. August" {
-        return NaiveDate::from_ymd(2019, 08, 5);
+fn get_date(day_and_month: &str, year: i32) -> NaiveDate {
+    let day_and_month: Vec<&str> = day_and_month
+        .split_terminator(".")
+        .map(|x| x.trim())
+        .collect();
+
+    let day: u32 = day_and_month[0].parse().unwrap();
+    let raw_month = day_and_month[1];
+
+    let month: u32;
+    match raw_month {
+        "Januar" => month = 1,
+        "Februar" => month = 2,
+        "März" => month = 3,
+        "April" => month = 4,
+        "Mai" => month = 5,
+        "Juni" => month = 6,
+        "Juli" => month = 7,
+        "August" => month = 8,
+        "September" => month = 9,
+        "Oktober" => month = 10,
+        "November" => month = 11,
+        "Dezember" => month = 12,
+        _ => month = 0,
     }
 
-    return NaiveDate::from_ymd(1970, 01, 01);
+    return NaiveDate::from_ymd(year, month, day);
+}
+fn get_dates_by_week_day(start: &str, end: &str, year: i32) -> HashMap<Weekday, NaiveDate> {
+    let start = get_date(start, year);
+    let end = get_date(end, year);
+
+    let days_in_between = end.signed_duration_since(start).num_days() - 1;
+
+    let mut dates_by_week_day = HashMap::new();
+    dates_by_week_day.insert(start.weekday(), start);
+    for n in 1..=days_in_between {
+        let current_date = start + Duration::days(n);
+        dates_by_week_day.insert(current_date.weekday(), current_date);
+    }
+    dates_by_week_day.insert(end.weekday(), end);
+    return dates_by_week_day;
 }
 
 pub fn get_menu_by_days(preformatted_menu: Vec<&str>) -> HashMap<NaiveDate, Vec<&str>> {
     let menu_by_day = HashMap::new();
+
+    let weekly_dishes = extract_weekly_dishes(preformatted_menu.to_owned());
+    let daily_dishes = extract_daily_dishes(preformatted_menu.to_owned());
+
     return menu_by_day;
 }
 
@@ -110,13 +151,31 @@ mod tests {
         assert!(result["Freitag"] == "Tintenfischtulpen mit Rosmarinkartoffeln  €  8,00");
     }
     #[test]
-    fn get_date_test() {
-        let result = get_date("5. Augusti", 2019);
+    fn get_date_5th_august_test() {
+        let result = get_date("5. August", 2019);
         let expected_result = NaiveDate::from_ymd(2019, 08, 5);
         assert_eq!(result, expected_result);
-        println!("{:?}", result);
+    }
+    #[test]
+    fn get_date_1st_january_test() {
+        let result = get_date("1. Januar", 2019);
+        let expected_result = NaiveDate::from_ymd(2019, 1, 1);
+        assert_eq!(result, expected_result);
+    }
+    #[test]
+    fn get_date_31_december_test() {
+        let result = get_date("31.Dezember", 2019);
+        let expected_result = NaiveDate::from_ymd(2019, 12, 31);
+        assert_eq!(result, expected_result);
     }
 
+    #[test]
+    fn get_dates_by_week_day_test() {
+        let result = get_dates_by_week_day("5. August", "9. August", 2019);
+        let expected_date_on_thursday = NaiveDate::from_ymd(2019, 08, 08);
+        let date_on_thursday = result[&Weekday::Thu];
+        assert_eq!(expected_date_on_thursday, date_on_thursday);
+    }
     /*
     #[test]
     fn get_menu_by_days_test() {
